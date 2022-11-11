e340cd5ce1d068fae7c2b698e400da21c163cc10
Piotr Sarna <sarna@chiselstrike.com>
2022-11-11 19:07:34 +0100

    Flask

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/generate', methods = ['POST'])
+def generate():
+    src = request.form['src']
+    if len(src) > 4096:
+        return "Max source code size is 4096 bytes"
+    print(src)
+    with open("libsql-target/input.rs", "w", encoding = "utf-8") as f:
+        f.write(src);
+    result = subprocess.run(['./generate.sh'], capture_output=True)
+    if result.returncode == 0:
+        return result.stdout.decode("utf-8")
+    else:
+        return result.stderr.decode("utf-8")
--- generate.sh
+++ generate.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+rustfmt libsql-target/input.rs
 analyzed=$(rust-code-analysis-cli -lrust -F -p libsql-target/input.rs | sed $'s,\x1b\\[[0-9;]*[a-zA-Z],,g')
 loc=$(wc -l libsql-target/input.rs | cut -d' ' -f1)
 fn_name=$(echo $analyzed | cut -d' ' -f 5 | tr -d ':')
@@ -9,6 +10,7 @@ fn_start=$(echo $analyzed | cut -d' ' -f 8)
 fn_end=$(echo $analyzed | cut -d' ' -f 11 | tr -d '.')
 
 if [[ "$fn_start" != "1" || "$fn_end" != "$loc" ]]; then
+  echo $fn_start $fn_end $loc
   echo "The source code should only contain a single function definition"
   exit 1
 fi
